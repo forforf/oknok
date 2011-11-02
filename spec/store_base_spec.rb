c3734512e7b53f78a5d2ba59c0555a063d04ce77
Dave M <dmarti21@gmail.com>
2011-11-02 15:47:16 +0000

    StoreBase updated and working

--- spec/store_base_spec.rb
+++ spec/store_base_spec.rb
@@ -6,21 +6,21 @@ module OknokConfigData
   Config = {
     'avail_stores' => {
       'couchstore' => {
-         'type' => 'couchdb',
-         'host' => 'couchsurfer.iriscouch.com', #=> easy to set up your own
-         'user' => nil
+        'type' => 'couchdb',
+        'host' => 'couchsurfer.iriscouch.com', #=> easy to set up your own
+        'user' => nil
       },
-     'local_filesystem' => {
+      'local_filesystem' => {
         'type' => 'file',
         'host' => '/tmp/spec/',
         'user' => nil
-     },
-     'local_filesystem_dup' => {
+      },
+      'local_filesystem_dup' => {
         'type' => 'file',
         'host' => '/tmp/spec/',
         'user' => nil
-     },
-     'remote_mysql' => {
+      },
+      'remote_mysql' => {
         'type' => 'mysql',
         'host_lookup_engine' => 'WebService',
         'host' => 'http://couchsurfer.iriscouch.com/ec2lookup/open_db',
@@ -37,19 +37,39 @@ module OknokConfigData
 
 end
 
-
 describe Oknok::StoreBase, "basic initialization tasks" do
   include Oknok
-
   
   before :each do
     config_data = OknokConfigData::Config["avail_stores"]
-    @couch_args    = ["couchstore", config_data["couchstore"]]
-    @file_args     = ["local_filesystem",config_data["local_filesystem"]]
-    @file_dup_args = ["local_filesystem_dup", config_data["local_filesystem_dup"]]
-    @mysql_args    = ["remote_mysql", config_data["remote_mysql"]]
-    @sdb_s3_args   = ["test_sdb_s3", config_data["test_sdb_s3"]]
-    @null_args     = ["any_store_name", {}]
+    @config_data = config_data
+    @type_to_class_map = {
+      'couchdb' => CouchDbStore,
+      'file' => FileStore,
+      'mysql' => MysqlStore,
+      'sdb_s3' => SdbS3Store,
+      :unknown => NullStore
+    }
+    #Map store handle to initialization data
+    #sto_handle => {:klass => StoreClass, :args => [sto_handle, sto_config_data] )
+    #For example
+    #@store_init_data = {..., 'file' => { :klass => FileStore, :args ['file', @config_data['file'] }, ...}
+    @store_init_data = @config_data.inject({}) do |memo, (k,v)|
+      memo[k] = {
+        :klass => @type_to_class_map[ v['type'] ],
+        :args => [k, @config_data[k]]
+      }
+      memo
+    end
+    
+    
+    #@couch_args    = ["couchstore", config_data["couchstore"]]
+    #@file_args     = ["local_filesystem",config_data["local_filesystem"]]
+    #@file_dup_args = ["local_filesystem_dup", config_data["local_filesystem_dup"]]
+    #@mysql_args    = ["remote_mysql", config_data["remote_mysql"]]
+    #@sdb_s3_args   = ["test_sdb_s3", config_data["test_sdb_s3"]]
+    #@null_args     = ["any_store_name", {}]
+      
     @class_data_map = {
       CouchDbStore => @couch_args,
       FileStore => @file_args,
@@ -59,56 +79,83 @@ describe Oknok::StoreBase, "basic initialization tasks" do
     }
   end
 
-  it "initializes from config data" do
+  it "initializes from config data without blowing up" do
     sto_objs = []
-    @class_data_map.each do |klass, args|
-      sto_objs << klass.new(*args)
+    @store_init_data.each do |sto_handle, sto_init_data|
+      sto_objs << sto_init_data[:klass].new(*sto_init_data[:args])
     end
     sto_objs
   end
-
-  it "has a store name" do
-    @store_objs.each_with_index do |sto, idx|
-      sto.store_name.should == @store_names[idx]
+ 
+  describe "after successful initialization" do
+  
+    before :each do
+      @store_obj_data_by_handle = @store_init_data.inject({}) do |memo, (sh,sd)|
+        memo[sh] = {
+          :store_obj => sd[:klass].new(*sd[:args]),
+          :config_data => sd[:args].last
+        }
+        memo
+      end
+      @store_objs = @store_obj_data_by_handle.inject([]) do |memo, (k,v)|
+        memo << v[:store_obj]
+        memo
+      end
     end
-  end
-
-  it "has an oknok name" do
-    @store_objs.each_with_index do |sto, idx|
-      sto.oknok_name.should == @oknok_name
+  
+    it "has a store type" do
+      @store_obj_data_by_handle.each do |sto_handle, sto_data|
+        sto_obj = sto_data[:store_obj]
+        sto_type = sto_data[:config_data]['type']
+        sto_obj.class.store_type.should == sto_type
+      end
     end
-  end
-
-  it "has host data" do
-    @store_objs.each do |sto|
-      sto_data = @all_store_data[sto.store_name]
-      sto.host.should == StoreNameLookup.config_reader(sto_data)
+   
+    it "has a store name" do
+      @store_obj_data_by_handle.each do |sto_handle, sto_data|
+        sto_obj = sto_data[:store_obj]
+        sto_obj.store_name.should == sto_handle
+      end
     end
-  end
+    
+    #it "has an oknok name" do
+    #  @store_objs.each_with_index do |sto, idx|
+    #    sto.oknok_name.should == @oknok_name
+    #  end
+    #end
 
-  it "has StoreAccess methods included" do
-    @store_objs.each do |sto|
-      sto.kind_of?(StoreAccess).should == true
+    it "has host data" do
+      @store_obj_data_by_handle.each do |sto_handle, sto_data|
+        sto_obj = sto_data[:store_obj]
+        sto_obj.host.should == StoreNameLookup.config_reader(sto_data[:config_data])
+      end
     end
-  end
 
-
-  it "has StoreAccess extended (class methods)" do
-    @store_objs.each do |sto|
-      sto.class.include?(StoreAccess).should == true
+    it "has StoreAccess methods included" do
+      @store_objs.each do |sto|
+        sto.kind_of?(StoreAccess).should == true
+      end
     end
-  end
 
-  it "collects instances" do
-    num_stores_in_config = @store_names.size
-    num_objs_made = @store_objs.size
-    num_objs_made.should == num_stores_in_config
-    collected_instances = StoreBase.get_my_instances
-    collected_instances.size.should == num_objs_made
+
+    it "has StoreAccess extended (class methods)" do
+      @store_objs.each do |sto|
+        sto.class.include?(StoreAccess).should == true
+      end
+    end
+
+    #moved to store_tracker
+    #it "collects instances" do
+    #  num_stores_in_config = @config_data.size
+    #  num_objs_made = @store_objs.size
+    #  num_objs_made.should == num_stores_in_config
+    #  collected_instances = StoreBase.get_my_instances
+    #  collected_instances.size.should == num_objs_made
+    #end
   end
 end
 
-
+=begin
 ##include StoreAccess shared tests
 #require_relative 'store_access_shared_spec'
 
@@ -164,3 +211,4 @@ describe Oknok::StoreBase, "Factory" do
 
 end
 
+=end
